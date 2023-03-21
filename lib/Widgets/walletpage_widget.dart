d02d3bbba57b708155093a06f4c13e6d21698595
mertoatakan <mertoatakan@gmail.com>
2023-03-21 16:39:04 +0300

    Wallet page is addded.

--- lib/Widgets/walletpage_widget.dart
+++ lib/Widgets/walletpage_widget.dart
@@ -7,13 +7,143 @@ class WalletPageState extends StatefulWidget {
 }
 
 class _WalletPageState extends State<WalletPageState> {
+  // sample data for coins and their values
+  final List<CoinData> coins = [
+    CoinData('Bitcoin', 'BTC', 1.5, 100000),
+    CoinData('Ethereum', 'ETH', 10.0, 50000),
+    CoinData('Binance Coin', 'BNB', 20.0, 75000),
+  ];
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Text(
-            'Wallet',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+  Widget build(BuildContext context) {
+    double totalValue = coins.fold(0, (sum, coin) => sum + coin.usdValue);
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(46, 21, 157, 1),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 140,
+            child: Container(
+              color: Color.fromRGBO(46, 21, 157, 0.6),
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.21),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Total Balance',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.05,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: screenWidth * 0.02),
+                    Text(
+                      '\$${totalValue.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.08,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
-      );
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(30),
+                ),
+                gradient: LinearGradient(
+                  colors: [Color.fromARGB(255, 255, 102, 0), Colors.orange],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: ListView.builder(
+                itemCount: coins.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 16.0,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  coins[index].name,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                  ),
+                                ),
+                                Text(
+                                  coins[index].symbol,
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  coins[index].amount.toStringAsFixed(4),
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                  ),
+                                ),
+                                Text(
+                                  '\$${coins[index].usdValue.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(
+                        height: 1,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CoinData {
+  final String name;
+  final String symbol;
+  final double amount;
+  final double price;
+
+  CoinData(this.name, this.symbol, this.amount, this.price);
+
+  double get usdValue => amount * price;
 }
